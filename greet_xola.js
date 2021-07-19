8333e4b4f12064f9a822d63bdf71badbfc12349e
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-07-20 00:15:49 +0200

    minor changes

--- greet_xola.js
+++ greet_xola.js
@@ -1,8 +1,13 @@
+"use strict"
 const greet = require('./greet');
 const chalk = require('chalk');
 const figlet = require('figlet');
 
-const styledMessage1 = figlet.textSync(greet('Xola'));
-console.log(styledMessage1);
-const styledMessage2 = chalk.bgGreen(greet('Xola'));
-console.log(styledMessage2);
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+figlet(greet("Xola"), (err, data)=>{
+	console.log(data)
+	return data
+})
+
+
+console.log(styledMessage)
