1d61ff89780852a585e2deb78a4d4501be03a784
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-07-19 18:04:38 +0200

    add chalk module

--- greet_xola.js
+++ greet_xola.js
@@ -2,5 +2,7 @@ const greet = require('./greet');
 const chalk = require('chalk');
 const figlet = require('figlet');
 
-const styledMessage = figlet.textSync(greet('Xola'));
-console.log(styledMessage);
+const styledMessage1 = figlet.textSync(greet('Xola'));
+console.log(styledMessage1);
+const styledMessage2 = chalk.bgGreen(greet('Xola'));
+console.log(styledMessage2);
